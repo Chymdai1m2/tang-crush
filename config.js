adb857f397a15880bd1e650790c6f1c3a3d7c609
Chymdai1m2 <89757958+Chymdai1m2@users.noreply.github.com>
2021-08-30 09:46:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
     introTitle: 'Em iu à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Hôm nay a có vài lời iu thương
+Mún gửi cho e mong rằng e chấp nhận 
+Và hãy trả lời thật lòng nha bời vì a rất là iu e 
+Và hãy iu mỗi riêng a thui nha`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    title: 'A đã iu e ngay lần đầu tiên , hihi mặc dù nói v hơi kì , nhưng là thật đó, Em có iu a không ?  😙',
+    desc: 'Phải trả lời thật lòng nha , nếu e tắt cái này thì đồng nghĩa với vc e từ chối  ',
+    btnYes: 'Dạ e iu a lắm <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    reply: 'Tại vì a rất dễ thương, đáng iu, đẹp trai, và a cho e cảm giác yên bình nhất  <33333333',
+    mess: 'Anh biết mà e iu  🥰. Yêu em nhiều nhiều , a mong tình iu của chúng ta kh bao h phai e nhé ! 😘😘',
     messDesc: 'Tối nay a sẽ gửi vitamin cho e iu nha .',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'Okiiiii lun, cảm ơn chồng iu của e  <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
